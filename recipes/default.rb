14bb4d560754295f3e82f5998e8fc52ed14079d5
Naila Nasleem <NNasleem@spartaglobal.com>
2019-08-14 09:19:04 +0100

    chef cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+apt_repository 'monogodb-org' do
+action :add
+uri  "http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse"
+components ['monogodb-org']
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -15,12 +15,18 @@ describe 'mongodb::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+  it 'runs apt get update' do
+    expect(chef_run).to update_apt_update 'update_sources'
   end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
+  it 'runs apt get update' do
+    expect(chef_run).to add_apt_repository 'monogodb-org'
+  end
+  # context 'When all attributes are default, on CentOS 7' do
+  #   # for a complete list of available platforms and versions see:
+  #   # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  #   platform 'centos', '7'
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
